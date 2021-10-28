e16734423096e0de8932c3b1e40c44ad32064d00
kkostiukov <kirill533@gmail.com>
2021-10-28 21:03:49 +0200

    Add README file with installation instructions. Add copyright notices.

--- Observer/ProductListCollection.php
+++ Observer/ProductListCollection.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * TopSort Magento Extension
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @copyright Copyright (c) TopSort 2021 - All Rights Reserved
+ * @author Kyrylo Kostiukov <kyrylo.kostiukov@bimproject.net>
+ * @license Proprietary
+ */
 namespace Topsort\Integration\Observer;
 
 use Magento\Catalog\Model\Product;
