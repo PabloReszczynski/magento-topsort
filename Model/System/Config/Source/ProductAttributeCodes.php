5ef5d3cabcc32928d28ef058f7f5d7d70686f38f
kkostiukov <kirill533@gmail.com>
2021-11-22 17:53:37 +0100

    Use dropdown list to select attribute codes

---
+++ Model/System/Config/Source/ProductAttributeCodes.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * TopSort Magento Extension
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @copyright Copyright (c) TopSort 2021 - All Rights Reserved
+ * @author Kyrylo Kostiukov <kyrylo.kostiukov@bimproject.net>
+ * @license Proprietary
+ */
+namespace Topsort\Integration\Model\System\Config\Source;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class ProductAttributeCodes
+{
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Eav\Attribute
+     */
+    private $attributeFactory;
+
+    function __construct(
+        \Magento\Catalog\Model\ResourceModel\Eav\Attribute $attributeFactory
+    )
+    {
+        $this->attributeFactory = $attributeFactory;
+    }
+
+    protected static $_options;
+
+    public function toOptionArray()
+    {
+
+        if (!self::$_options) {
+
+            $attributeInfo = $this->attributeFactory->getCollection();
+            $attributeInfo->addOrder('attribute_code');
+            $attributes = [];
+            foreach($attributeInfo as $attribute)
+            {
+                /** @var \Magento\Catalog\Model\ResourceModel\Eav\Attribute $attribute */
+                if ($attribute->getFrontendInput() === 'select') {
+                    $attributeCode = $attribute->getAttributeCode();
+                    $attributes[] = [
+                        'label' => $attribute->getDefaultFrontendLabel() . ' (' . $attributeCode . ')',
+                        'value' => $attributeCode
+                    ];
+                }
+            }
+
+            self::$_options = $attributes;
+        }
+
+        return self::$_options;
+    }
+
+}
